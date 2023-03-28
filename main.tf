7820b4407751ce9df64ceb258c17bb6538d72c74
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-03-28 07:29:48 +0000

    fixes

--- main.tf
+++ main.tf
@@ -41,3 +41,9 @@ resource "aws_route53_key_signing_key" "primary" {
   status                     = "ACTIVE"
 }
 
+resource "aws_route53_hosted_zone_dnssec" "primary" {
+  depends_on = [
+    aws_route53_key_signing_key.primary
+  ]
+  hosted_zone_id = aws_route53_key_signing_key.primary.hosted_zone_id
+}
