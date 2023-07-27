074d37005ab7eda6d9baf71df4cc4b0cdbe853a2
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-07-27 15:22:50 -0700

    Feat/adding redirects for top level domain (#2)
    * chore: run terraform fmt
    * feat: adding redirects for top level apex domain and www to glueops.dev
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ redirects.tf
@@ -0,0 +1,24 @@
+
+locals {
+  redirect_url = "https://glueops.dev"
+}
+
+module "redirect_apex_domain" {
+  source = "git::https://github.com/GlueOps/terraform-module-cloud-aws-route53-s3-http-redirect.git?ref=v0.1.2-glueops"
+  providers = {
+    aws = aws.management-tenant-dns
+  }
+  zone_name           = aws_route53_zone.domain.name
+  domain_name         = aws_route53_zone.domain.name
+  redirect_target_url = local.redirect_url
+}
+
+module "redirect_www_domain" {
+  source = "git::https://github.com/GlueOps/terraform-module-cloud-aws-route53-s3-http-redirect.git?ref=v0.1.2-glueops"
+  providers = {
+    aws = aws.management-tenant-dns
+  }
+  zone_name           = aws_route53_zone.domain.name
+  domain_name         = "www.${aws_route53_zone.domain.name}"
+  redirect_target_url = local.redirect_url
+}
